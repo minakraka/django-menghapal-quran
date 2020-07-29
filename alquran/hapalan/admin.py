bab9516acd9ba1824e6561443ed50484a683270b
minakraka <rakanurpandi@gmail.com>
2020-07-29 08:35:23 +0700

    register halaman admin

--- alquran/hapalan/admin.py
+++ alquran/hapalan/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Qory, Surat, Ayat
 
 # Register your models here.
+class SuratAdmin(admin.ModelAdmin):
+    list_display = ('no_surat', 'nama_surat')
+admin.site.register(Surat, SuratAdmin)
+
+class AyatAdmin(admin.ModelAdmin):
+    list_display = ('no_ayat', 'url', 'surat')
+admin.site.register(Ayat, AyatAdmin)
+
+class QoryAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'profile')
+admin.site.register(Qory, QoryAdmin)
